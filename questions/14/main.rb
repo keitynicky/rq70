2b750f3388d375fdcb5457e8f4586753e3dc56c8
nana <keyaki.436vrwgz@gmail.com>
2016-08-23 20:48:14 +0900

    Ruby勉強中

---
+++ questions/14/main.rb
@@ -0,0 +1,77 @@
+require 'benchmark'
+
+module Q14
+  module_function
+
+  @country = [
+    "Australia",
+    "Iran",
+    "Japan",
+    "South Korea",
+    "Algeria",
+    "Cameroon",
+    "Ghana",
+    "Ivory Coast",
+    "Nigeria",
+    "Costa Rica",
+    "Honduras",
+    "Mexico",
+    "United States",
+    "Argentina",
+    "Brazil",
+    "Chile",
+    "Colombia",
+    "Ecuador",
+    "Uruguay",
+    "Belgium",
+    "Bosnia and Herzegovina",
+    "Croatia",
+    "England",
+    "France",
+    "Germany",
+    "Greece",
+    "Italy",
+    "Netherlands",
+    "Portugal",
+    "Russia",
+    "Spain",
+    "Switzerland",
+    "USA"]
+  
+  @is_used = Array.new(@country.size, false)
+
+  def run
+    @max_depth = 0
+    @country.each_with_index{|c, i|
+      @is_used[i] = true
+      search(c, 1)
+      @is_used[i] = false      
+    }
+
+    @max_depth
+  end
+
+  def search(prev, depth)
+    is_last = true
+    @country.each_with_index{|c, i|
+      if c[0] == prev[-1].upcase then
+        if !@is_used[i] then
+          is_last = false
+          @is_used[i] = true
+          search(c, depth + 1)
+          @is_used[i] = false  
+        end
+      end
+    }   
+    @max_depth = [@max_depth, depth].max if is_last 
+  end
+end
+
+Benchmark.bm do |x|
+  x.report do
+    $answer = Q14.run
+  end
+end
+
+puts
+puts "answer : #{$answer}"
