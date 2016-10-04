b0f53f5f0f93d66239dbea00216a8fb126ea69f2
nana <keyaki.436vrwgz@gmail.com>
2016-10-04 20:44:20 +0900

    Ruby勉強中

--- questions/19/main.rb
+++ questions/19/main.rb
@@ -1,10 +1,30 @@
 require 'benchmark'
+require 'prime'
 require "pry"
 
 module Q19
   module_function
 
+  TARGET_COUNT = 6
+  SQUARE = 2
+
+  # 写経！
   def run
+    # Primeは素数を扱うクラス。そういうのがあるんだね！
+    primes = Prime.take(TARGET_COUNT)
+    min = primes.last ** SQUARE
+    min_friend = []
+
+    primes.permutation{|prime|
+      friends = prime.each_cons(2).map{|x, y| x * y}
+      friends += [prime.first, prime.last].map{|x| x * x}
+      if min > friends.max 
+        min = friends.max
+        min_friend = friends
+      end
+    }
+
+    min
   end
 
 end
