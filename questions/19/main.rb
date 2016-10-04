f68a9846888fa886738426d7e14b794d1fa995c8
nana <keyaki.436vrwgz@gmail.com>
2016-10-04 21:10:49 +0900

    Ruby勉強中

--- questions/19/main.rb
+++ questions/19/main.rb
@@ -7,17 +7,21 @@ module Q19
 
   TARGET_COUNT = 6
   SQUARE = 2
+  PEAR = 2
 
   # 写経！
   def run
-    # Primeは素数を扱うクラス。そういうのがあるんだね！
+    # Primeは素数を扱うクラス。そういうのがあるんだね！[互いに素な数]を取得するために。
     primes = Prime.take(TARGET_COUNT)
     min = primes.last ** SQUARE
     min_friend = []
 
+    # 対象となるデータの順列
     primes.permutation{|prime|
-      friends = prime.each_cons(2).map{|x, y| x * y}
-      friends += [prime.first, prime.last].map{|x| x * x}
+      # ２つの数の掛け算できる数の組み合わせで計算(ここはもう少しシンプルに書けないのかな？injectとか使って。。)
+      friends = prime.each_cons(PEAR).map{|x, y| x * y}
+      # 先頭と末尾は同じ数の平方
+      friends += [prime.first, prime.last].square!
       if min > friends.max 
         min = friends.max
         min_friend = friends
@@ -27,6 +31,12 @@ module Q19
     min
   end
 
+end
+
+class Array
+  def square!
+    self.map! {|num| num ** SQUARE}
+  end
 end
 
 Benchmark.bm do |x|
