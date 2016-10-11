2fdd6adb6e2cccaa675dd32602333291668a2471
nana <keyaki.436vrwgz@gmail.com>
2016-10-11 20:41:53 +0900

    Ruby勉強中

--- questions/20/main.rb
+++ questions/20/main.rb
@@ -3,8 +3,22 @@ require "pry"
 
 module Q20
   module_function
+  
+  FACADE_OF_THE_PASSION = [1,14,14,4,11,7,6,9,8,10,10,5,13,2,3,15]
 
   def run
+    combination_count = Hash.new(0)
+    (1..FACADE_OF_THE_PASSION.length).each{|i|
+      FACADE_OF_THE_PASSION.combination(i).each{|items|
+        tmp = items.inject(:+)
+        combination_count[tmp] += 1
+      }
+    }
+    largest_hash_key combination_count
+  end
+
+  def largest_hash_key(hash)
+    hash.max_by{|k,v| v}
   end
 
 end
