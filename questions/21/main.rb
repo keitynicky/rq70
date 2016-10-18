50e585af72a7a59f9077071819210da14691f2ae
nana <keyaki.436vrwgz@gmail.com>
2016-10-18 20:57:07 +0900

    答え合わせまで実施。残りの時間でリファクタ

--- questions/21/main.rb
+++ questions/21/main.rb
@@ -4,7 +4,33 @@ require "pry"
 module Q21
   module_function
 
+  LAST_COUNT = 2014
+  XOR_INPUT = 2
+
   def run
+    row_index = 0
+    count = 0
+    xor_rows = []
+    while count < LAST_COUNT
+      row_index += 1
+      xor_rows.push get_row(xor_rows.last)
+      count += xor_rows.last[1..-2].count{|item| item == 0}
+    end
+    row_index
+  end
+
+  def get_row before_row
+    row = []
+    row.push 0
+    if before_row.nil?
+      row.push 1
+    else
+      before_row.each_cons(XOR_INPUT).each{|xor_pair|
+        row.push xor_pair.first ^ xor_pair.last
+      }
+    end
+    row.push 0
+    row      
   end
 
 end
