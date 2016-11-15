9b873ed26b3dfadac06db66e54821b46120616fc
nana <keyaki.436vrwgz@gmail.com>
2016-11-15 10:42:33 +0900

    リファクタ

--- questions/24/main.rb
+++ questions/24/main.rb
@@ -4,6 +4,8 @@ require 'pry'
 module Q24
   module_function
 
+  NO_STRIKE_OUT_NUM = 5
+
   def run
     count = (1..9).to_a.permutation.count
     count += strike_out_count
@@ -14,13 +16,13 @@ module Q24
     0
   end
 
-  def strike_out_pairs
+  def all_strike_out_pairs
     items = (1..9).each_slice(3)
-    (hoge(items) + hoge(items.to_a.transpose)).reject{|p| p.include?(5)}
+    (strike_out_pairs(items) + strike_out_pairs(items.to_a.transpose)).reject { |p| p.include?(NO_STRIKE_OUT_NUM) }
   end
 
-  def hoge items
-    items.map{|p| p.each_cons(2).to_a}.flatten(1)
+  def strike_out_pairs(items)
+    items.flat_map { |p| p.each_cons(2).to_a }
   end
 end
 
