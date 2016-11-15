eae728902e24d2fc2ecbc23fce7bb58366e07f66
nana <keyaki.436vrwgz@gmail.com>
2016-11-15 11:48:47 +0900

    コード実装。remote.rbの際にはリファクタ等の対応をする

--- questions/24/main.rb
+++ questions/24/main.rb
@@ -7,21 +7,32 @@ module Q24
   NO_STRIKE_OUT_NUM = 5
 
   def run
-    count = (1..9).to_a.permutation.count
-    count += strike_out_count
-    binding.pry
+    strike_out_count
   end
 
   def strike_out_count
-    0
+    strike_outs = get_strike_outs + (1..9).each_cons(1).to_a
+    @memo = { [] => 1 }
+    total_count(strike_outs)
+    # binding.pry
   end
 
-  def all_strike_out_pairs
+  def total_count(strike_outs)
+    return @memo[strike_outs] if @memo.key?(strike_outs)
+    count = 0
+    strike_outs.each do |p|
+      tmp = strike_outs.select { |item| (item & p).empty? }
+      count += total_count(tmp)
+    end
+    @memo[strike_outs] = count
+  end
+
+  def get_strike_outs
     items = (1..9).each_slice(3)
-    (strike_out_pairs(items) + strike_out_pairs(items.to_a.transpose)).reject { |p| p.include?(NO_STRIKE_OUT_NUM) }
+    (get_pairs(items) + get_pairs(items.to_a.transpose)).reject { |p| p.include?(NO_STRIKE_OUT_NUM) }
   end
 
-  def strike_out_pairs(items)
+  def get_pairs(items)
     items.flat_map { |p| p.each_cons(2).to_a }
   end
 end
