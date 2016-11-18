ea90d66d47b2e2bb29557089fe1e259db5156fad
nana <keyaki.436vrwgz@gmail.com>
2016-11-18 18:49:52 +0900

    ユニットテスト作成

--- questions/25/spec/main_spec.rb
+++ questions/25/spec/main_spec.rb
@@ -26,6 +26,44 @@ describe "Q25" do
         x = Stocker.new [[]], [[1,1]]
         expect(x.current_is_same?).to be_truthy
     end
+
+    describe "reject_index" do
+        it "同じ値でなく、first" do
+            tmp = extended_class.start_target 1
+            x = Stocker.new tmp, [tmp.first]
+            expect(x.reject_index).to eq 0
+        end
+
+        it "同じ値でなく、last" do
+            tmp = extended_class.start_target 1
+            x = Stocker.new tmp[2], tmp[0..1]
+            expect(x.reject_index).to eq -1
+        end
+
+        it "同じ値で、first" do
+            x = Stocker.new [[2,2]], [[0,1],[1,0],[1,2]]
+            expect(x.reject_index).to eq 0
+        end
+
+        it "同じ値で、last" do
+            x = Stocker.new [[2,2]], [[0,1],[1,0],[2,1]]
+            expect(x.reject_index).to eq -1
+        end
+    end
+
+    describe "candidates" do
+        it "穴の数が2つで初回" do
+            tmp = extended_class.start_target 1
+            x = Stocker.new tmp, [tmp.first]
+            expect(x.candidates).to eq [[1,0],[1,1]]
+        end
+
+        it "穴の数が3つで初回" do
+            tmp = extended_class.start_target 2
+            x = Stocker.new tmp, [tmp.first]
+            expect(x.candidates).to eq [[1,0],[1,1],[1,2],[2,0],[2,1],[2,2]]
+        end
+    end
   end
 
 end
