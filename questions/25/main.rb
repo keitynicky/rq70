9bca37d00277f182c323beb9215cd71356e684e0
nana <keyaki.436vrwgz@gmail.com>
2016-11-20 19:53:37 +0900

    リファクタ

--- questions/25/main.rb
+++ questions/25/main.rb
@@ -12,23 +12,27 @@ module Q25
     answer candidates HOLES
   end
 
-  def candidates holes
+  def candidates(holes)
     (1..holes - 1).to_a.permutation(holes - 1).to_a
   end
 
-  def answer permutation
+  def answer(permutation)
     ans = [0, []]
     permutation.product(permutation) do |set|
-      candidates = set.transpose.flatten
-      shoelace = [[0, candidates.first]]
-      candidates.each_index do |i|
-        shoelace << [candidates[i], (i + 1) == candidates.size ? 0 : candidates[i + 1]]
-        shoelace[-1] = shoelace.last.reverse if i.even?
-      end
+      shoelace = hoge set.transpose.flatten
       c = cross_count shoelace
       ans = [c, shoelace] if c > ans.first
     end
     ans
+  end
+
+  def hoge candidates
+    shoelace = [[0, candidates.first]]
+    candidates.each_index do |i|
+      shoelace << [candidates[i], (i + 1) == candidates.size ? 0 : candidates[i + 1]]
+      shoelace[-1] = shoelace.last.reverse if i.even?
+    end
+    shoelace
   end
 
   def cross_count(l)
@@ -42,10 +46,9 @@ module Q25
     line2_y1, line2_y2 = line2
     x1 = 0
     x2 = 1
-    ((x1 - x2) * (line2_y1 - line1_y1) + (line1_y1 - line1_y2) * (x1 - x1)) * ((x1 - x2) * (line2_y2 - line1_y1) + (line1_y1 - line1_y2) * (x1 - x2)) < 0 && 
-    ((x1 - x2) * (line1_y1 - line2_y1) + (line2_y1 - line2_y2) * (x1 - x1)) * ((x1 - x2) * (line1_y2 - line2_y1) + (line2_y1 - line2_y2) * (x1 - x2)) < 0
+    ((x1 - x2) * (line2_y1 - line1_y1) + (line1_y1 - line1_y2) * (x1 - x1)) * ((x1 - x2) * (line2_y2 - line1_y1) + (line1_y1 - line1_y2) * (x1 - x2)) < 0 &&
+      ((x1 - x2) * (line1_y1 - line2_y1) + (line2_y1 - line2_y2) * (x1 - x1)) * ((x1 - x2) * (line1_y2 - line2_y1) + (line2_y1 - line2_y2) * (x1 - x2)) < 0
   end
-
 end
 
 Benchmark.bm do |x|
