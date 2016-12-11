7845f185ded5607812e78d8821fe51ee93de12cf
nana <keyaki.436vrwgz@gmail.com>
2016-12-11 13:00:09 +0900

    rubocop対応

--- questions/26/main.rb
+++ questions/26/main.rb
@@ -8,19 +8,17 @@ module Q26
   HEIGHT = 10
 
   def run
-    # 空白スペースを左上までもっていくステップ数 
+    # 空白スペースを左上までもっていくステップ数
     step_going = WIDTH + HEIGHT - 2
     # 左上の車を右下まで持っていくステップ数
-    step_return =(WIDTH - 1 + HEIGHT - 2) * 3
+    step_return = (WIDTH - 1 + HEIGHT - 2) * 3
     # 問題には幅優先探索を用いてとありましたが、最短ルートを求めるので上の法則でもいいのかなと思いました。
     step_going + step_return
   end
-
 end
 
 Benchmark.bm do |x|
   x.report do
-
     $answer = Q26.run
     $correct = 69
   end
