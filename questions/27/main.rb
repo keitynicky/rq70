180be9ba034f5e0418b712a4d9d1788f43345125
nana <keyaki.436vrwgz@gmail.com>
2016-12-19 17:52:38 +0900

    rubocop対応

--- questions/27/main.rb
+++ questions/27/main.rb
@@ -5,15 +5,15 @@ module Q27
   module_function
 
   WIDTH = 6
-  # WIDTH = 3
+  # # WIDTH = 3
   HEIGHT = 4
-  # HEIGHT = 2
+  # # HEIGHT = 2
 
   def run
     start = 'r'
-    fuga start, [[[0,0],[1,0]]]
+    fuga start, [[[0, 0], [1, 0]]]
     memo.length
-    # binding.pry
+    # # binding.pry
   end
 
   def memo
@@ -23,17 +23,14 @@ module Q27
   def fuga(next_d, line)
     next_directions(next_d).each do |d|
       tmp = hoge d, line.last.last
-      if can_use? tmp, line
-        l = Array.new(line)
-        l << tmp
-        if is_goal? tmp.last
-          unless memo.include? l
-            @memo << l
-          end
-        else
-          fuga d, l
-        end
-      end      
+      next unless can_use? tmp, line
+      l = Array.new(line)
+      l << tmp
+      if is_goal? tmp.last
+        @memo << l unless memo.include? l
+      else
+        fuga d, l
+      end
     end
   end
 
@@ -50,15 +47,15 @@ module Q27
     tmp
   end
 
-  def can_use? next_line, line
-    in_range?(next_line.last) && !already_used?(next_line, line) && next_line.last != [0,0] && line.last.last != [WIDTH, HEIGHT]
-  end 
+  def can_use?(next_line, line)
+    in_range?(next_line.last) && !already_used?(next_line, line) && next_line.last != [0, 0] && line.last.last != [WIDTH, HEIGHT]
+  end
 
   def in_range?(position)
     position[0] <= WIDTH && position[-1] <= HEIGHT && position[0] >= 0 && position[-1] >= 0
   end
 
-  def already_used? next_line, line
+  def already_used?(next_line, line)
     line.include?(next_line) || line.include?(next_line.reverse)
   end
 
